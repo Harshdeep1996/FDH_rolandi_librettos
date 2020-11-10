4ff4ca0b846ff1ce3583d0aa3367c53ea37ffcb0
ludovicaschaerf <ludovica.schaerf@gmail.com>
2020-11-10 20:12:11 +0100

    theater latitude and longitude

--- title_metadata_extraction.py
+++ title_metadata_extraction.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import re
+from geopy.geocoders import Nominatim
+key = 'AIzaSyDO9Hf9LcmDIgXH_jB4pUlMcogZpYcr860'
+geolocator = Nominatim(user_agent=key)
 
 locations = pd.read_csv('./data/librettos.csv', delimiter='	')
 
@@ -10,12 +13,29 @@ expr = r'''dramma per musica|dramma giocoso|farsa giocosa|tragedia lirica|pastor
 locations['genre_opera'] = [re.search(expr,s.lower()).group(0) if re.search(expr,s.lower()) else 'Not found' for s in locations.title]
 
 place = r'teatro|chiesa|cappella|oratorio|theatre|casa|nozze|sala|carnevale|theatro' 
-time = r"l'anno|dell'anno|del \d+|\d+|\,"
+time = r"anno|l'anno|l'autunno|la primavera|l'estate|l'inverno|dell'anno|il novembre|l'auttuno|ne' mesi di|nel mese di|per la prima opera del|per la fiera|nel prossimo|nel|nella|nell'occasione|con l'occasione|nella stagione di|la stagione|nell'està|in occasione|per la|per il|nel presente|del \d+|carnovale|\d+|\,|il "
 locations['location'] = [re.split(time, re.split(place,s.lower())[1])[0] if len(re.split(place,s.lower())) > 1 else 'Not found' for s in locations.title]
 
 locations['location_type'] = [re.search(place,s.lower()).group(0) if re.search(place,s.lower()) else 'Not found' for s in locations.title]
+locations['location_full'] = locations['location_type'] + locations['location']
 
 composer = r'per musica di|maestro|posta in musica dal|musica del sig\.|musica del signor|musica è del sig\.|musica è del signor|musicata da'
 locations['composer'] = [s[re.search(composer,s.lower()).span(0)[0] : re.search(composer,s.lower()).span(0)[1] + 20]  if re.search(composer,s.lower()) else 'Not found' for s in locations.title]
+
+def get_latitude(x):
+    try:
+        return x.latitude
+    except:
+        return 'Not found'
+
+def get_longitude(x):
+    try:
+        return x.longitude
+    except:
+        return 'Not found'
+
+geolocate_column = locations['location_full'].apply(geolocator.geocode)
+locations['location_latitude'] = geolocate_column.apply(get_latitude)
+locations['location_longitude'] = geolocate_column.apply(get_longitude)
 
 locations.to_csv('./data/librettos_1.csv')
